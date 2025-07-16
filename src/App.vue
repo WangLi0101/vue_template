15c1da5769cb965b978176c161cd8c503d4b224a
wang li <1366197226@qq.com>
2025-07-16 22:22:52 +0800

    feat:init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
-    <Header />
     <router-view />
   </div>
 </template>
 
-<script setup lang="ts">
-import Header from './components/header.vue';
-</script>
+<script setup lang="ts"></script>
--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -1,20 +1,5 @@
 <template>
-  <div class="home">
-    <video class="w-[200px] h-[200px]" id="videoPlayer" />
-  </div>
+  <div class="home">11</div>
 </template>
 
-<script setup lang="ts">
-import { useTemporyStore } from '@/store/tempory';
-import { emitter } from '@/utils/mitt';
-import { watch } from 'vue';
-const temporyStore = useTemporyStore();
-watch(
-  () => temporyStore.isStartVideo,
-  (newVal) => {
-    if (newVal) {
-      emitter.emit('player', 'videoPlayer');
-    }
-  }
-);
-</script>
+<script setup lang="ts"></script>
