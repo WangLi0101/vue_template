19889f303c240326580a16e2ff1ed4e356d38d94
wang li <1366197226@qq.com>
2025-08-05 20:51:31 +0800

    feat: 添加TRTC SDK支持并实现本地视频流初始化
    添加trtc-js-sdk依赖并实现本地视频流初始化功能
    配置Vite服务器支持HTTPS和跨域
    更新pnpm-lock.yaml文件

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -1,20 +1,53 @@
 <template>
   <div class="home">
-    <button @click="start">start</button>
-    <button @click="stop">stop</button>
-    <p class="bg-amber-400">
-      {{ timeStore.seconds }}
-    </p>
+    <div id="local_stream" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { useTimeStore } from "@/store/time";
-const timeStore = useTimeStore();
-const start = () => {
-  timeStore.start(1);
+import { onMounted } from "vue";
+import TRTC from "trtc-js-sdk";
+onMounted(() => {
+  getInputList();
+  initStream();
+});
+const getInputList = (): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    TRTC.getCameras().then(devices => {
+      if (devices.length === 0) {
+        reject("没有摄像头");
+      }
+      const res = devices.find(device => {
+        return device.getCapabilities().facingMode?.includes("user");
+      });
+      if (res) {
+        resolve(res.deviceId);
+      } else {
+        resolve(devices[0].deviceId);
+      }
+    });
+  });
 };
-const stop = () => {
-  timeStore.stop();
+
+const initStream = async () => {
+  const deviceId = await getInputList();
+  console.log(deviceId);
+
+  const localStream = TRTC.createStream({
+    userId: "1212121",
+    audio: true,
+    video: true,
+    cameraId: deviceId
+  });
+  try {
+    await localStream.initialize();
+    //播放本地流
+    localStream.play("local_stream");
+    console.log("初始化本地流成功");
+  } catch (error) {
+    console.error("初始化本地流失败 " + error);
+  }
 };
 </script>
+
+<style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
-
+import fs from "fs";
 /** 路径查找 */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -20,5 +20,18 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias
+  },
+  server: {
+    // 允许跨域
+    cors: true,
+    // 允许自定义端口
+    port: 8080,
+    // 允许自定义主机
+    host: "0.0.0.0",
+    // 开启https
+    https: {
+      key: fs.readFileSync("192.168.0.13-key.pem"),
+      cert: fs.readFileSync("192.168.0.13.pem")
+    }
   }
 });
